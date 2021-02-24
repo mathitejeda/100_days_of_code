4acebf260e85f8c66cf0d76312a70f717f14ba04
Mathias Tejeda <mathi.tejeda@gmail.com>
2021-02-24 20:51:02 -0300

    NATO alphabet using pandas and comprehension methods: added exceptions handling

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -3,8 +3,14 @@ import pandas
 NATO_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 NATO_alphabet = {row.letter: row.code for (index, row) in NATO_df.iterrows()}
 
+def generate_phonetic():
+    word = input("Write a name to create your phonetic code words list: ").upper()
+    try:
+        PCW = [NATO_alphabet[letter] for letter in word]
+    except KeyError:
+        print("Sorry, you can only type letters")
+        generate_phonetic()
+    else:
+        print(PCW)
 
-word = input("Write a name to create your phonetic code words list: ").upper()
-
-PCW = [NATO_alphabet[letter] for letter in word]
-print(PCW)
+generate_phonetic()
