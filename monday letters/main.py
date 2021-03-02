63079ef3ef5db52cba33345fcc21bfac77b25a4c
Mathias Tejeda <mathi.tejeda@gmail.com>
2021-03-01 21:08:01 -0300

    monday letters automation

---
+++ monday letters/main.py
@@ -0,0 +1,20 @@
+import smtplib
+import datetime as dt
+import random
+quotelist = []
+date = dt.datetime.now()
+
+with open("quotes.txt") as quote_data:
+    quotelist = quote_data.read().splitlines()
+print(quotelist)
+
+email = "1000tonic@gmail.com"
+password = "9S&ljY8kqgFN$%"
+
+if date.weekday() == 0:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        connection.sendmail(from_addr=email,
+                            to_addrs="miltonicc@yahoo.com",
+                            msg=f"Subject: Frase del lunes\n\nAca esta tu frase del lune: \n{random.choice(quotelist)}")
