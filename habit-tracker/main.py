26954aa0c2b27e337234bbc47d57b6dd17c4e0fa
Mathias Tejeda <mathi.tejeda@gmail.com>
2021-03-16 11:15:47 -0300

    Habit tracker using the pixela api, functions to update, upload and create graphics

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -1,18 +1,49 @@
 import requests
+from datetime import datetime
 
 pixela_endpoint = "https://pixe.la/v1/users"
 USER = ""
 TOKEN = ""
 
-
 params = {
-    "token":TOKEN,
+    "token": TOKEN,
     "username": USER,
     "agreeTermsOfService": "yes",
-    "notMinor":"yes",
+    "notMinor": "yes",
 }
 
 # response = requests.post(url=pixela_endpoint,json=params)
 # print(response.text)
 
-graph_endpoint =
+# graph_params = {
+#     "id": "",
+#     "name": "",
+#     "unit": "",
+#     "type": "",
+#     "color": "",
+# }
+#
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+#
+# graph_endpoint = f"{pixela_endpoint}/{USER}/graphs"
+#
+# response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+# print(response.text)
+
+post_endpoint = f"{pixela_endpoint}/{USER}/graphs/<graph>"
+
+today = datetime(day=15, month=3, year=2021)
+
+post_body = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "120",
+}
+
+response = requests.post(url=post_endpoint, json=post_body, headers=headers)
+
+# update_endpoint = f"{pixela_endpoint}/{USER}/graphs/<graph>/20210315"
+#
+# response = requests.put(url=update_endpoint,json=post_body,headers=headers)
+# print(response.text)
